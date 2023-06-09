368d64de7ee5955f7f3497d1ebc8b0f407479836
Stoeffler Mario <s52430@edu.campus02.at>
2023-06-09 12:49:58 +0200

    player moving now;

--- core/src/main/java/at/campus02/swd/game/Main.java
+++ core/src/main/java/at/campus02/swd/game/Main.java
@@ -3,6 +3,8 @@ package at.campus02.swd.game;
 import at.campus02.swd.game.factory.*;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -12,117 +14,148 @@ import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 import at.campus02.swd.game.gameobjects.GameObject;
 import at.campus02.swd.game.input.GameInput;
+import at.campus02.swd.game.gameobjects.Player;
 
-public class Main extends ApplicationAdapter {
+public class Main extends ApplicationAdapter implements InputProcessor {
     private SpriteBatch batch;
-
     private ExtendViewport viewport = new ExtendViewport(480.0f, 480.0f, 480.0f, 480.0f);
-    // Der Viewport für die Darstellung des Spiels
-    private GameInput gameInput = new GameInput();
-    // Das GameInput-Objekt für die Spielersteuerung
+    private GameInput gameInput;
     private Array<GameObject> gameObjects = new Array<>();
-    // Eine Array-Liste von GameObjects, die im Spiel vorhanden sind
     private final float updatesPerSecond = 60;
-    // Anzahl der Logikaktualisierungen pro Sekunde
     private final float logicFrameTime = 1 / updatesPerSecond;
-    // Dauer eines Logik-Frames in Sekunden
     private float deltaAccumulator = 0;
-    // Akkumulator für die Zeitdifferenz zwischen den Render-Aufrufen
     private BitmapFont font;
-    // Ein BitmapFont-Objekt für die Textdarstellung im Spiel
-
-    private GameObject tile;
-    // Eine Referenz auf ein Spielobjekt vom Typ Tile
-
-    private GameObject player;
-    // Eine Referenz auf ein Spielobjekt vom Typ Player
-
-///
-
-
+    private Player player;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
 
-        // Erzeugt einen neuen SpriteBatch für die Darstellung von Sprites
+        Factory tileFactory = new TileFactory();
+        PlayerFactory playerFactory = new PlayerFactory();
 
-        Factory tileFactory = new TileFactory();// Eine Fabrik zum Erstellen von Tile-Objekten
-        PlayerFactory player = new PlayerFactory();// Eine Fabrik zum Erstellen von Player-Objekten
-        // Erzeugt eine Anordnung von MeadowCenter-Tiles basierend auf der Viewport-Größe
         for (int i = 0; i < (viewport.getMinWorldHeight() / 64); i++) {
             for (int y = 0; y < (viewport.getMinWorldHeight() / 64); y++) {
-                gameObjects.add(tileFactory.create(Type.MEADOW_CENTER,i*64,y*64));
+                gameObjects.add(tileFactory.create(Type.MEADOW_CENTER, i * 64, y * 64));
             }
         }
 
         MeadowBuilder mb = new MeadowBuilder();
-        for (GameObject o:mb.placeTile(-160,-160,5,5)
-        ) {
+        for (GameObject o : mb.placeTile(-160, -160, 5, 5)) {
             gameObjects.add(o);
         }
 
-        /*
-        for(int x = -240; x<240; x+=64) {
-            for (int y = 240; y > -240; y -= 64) {
-                tile = new Tile();
-                tile.setPosition(x,y-64);
-                gameObjects.add(tile);
-            }
-        }
-*/
-        // Fügt ein Player-Objekt an der Position (0, 0) hinzu
-        gameObjects.add(player.create(Type.PLAYER,0,0));
-        // Erzeugt ein neues BitmapFont-Objekt für die Textdarstellung
-        font = new BitmapFont();
-        font.setColor(Color.WHITE); // Setzt die Farbe des Textes auf Weiß
-        Gdx.input.setInputProcessor(this.gameInput); // Setzt den GameInput-Processor für die Spielersteuerung
+        player = (Player) playerFactory.create(Type.PLAYER, 0, 0);
+        gameObjects.add(player);
 
+        gameInput = new GameInput(player);
+
+        Gdx.input.setInputProcessor(this.gameInput);
+
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        Gdx.input.setInputProcessor(this.gameInput);
     }
 
-   private void act(float delta) {
-        for(GameObject gameObject : gameObjects) {
-           gameObject.act(delta);
-       }
-   }
+    private void act(float delta) {
+        for (GameObject gameObject : gameObjects) {
+            gameObject.act(delta);
+        }
+        player.act(delta); // Hier die act-Methode des Spielers aufrufen
+    }
 
     private void draw() {
-        batch.setProjectionMatrix(viewport.getCamera().combined);// Setzt die Projektionsmatrix
-        // des SpriteBatch auf die kombinierte Matrix der Viewport-Kamera
-        batch.begin(); // Beginnt das Zeichnen mit dem SpriteBatch
-        for(GameObject gameObject : gameObjects) {
-            gameObject.draw(batch);// Zeichnet jedes Spielobjekt mit dem SpriteBatch
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+        for (GameObject gameObject : gameObjects) {
+            gameObject.draw(batch);
         }
-        font.draw(batch, "Hello Game", -220, -220);// Zeichnet den Text "Hello Game" an der angegebenen Position
-        batch.end();// Beendet das Zeichnen mit dem SpriteBatch
+        font.draw(batch, "Hello Game", -220, -220);
+        batch.end();
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(0.52f, 0.81f, 0.92f, 1f);
-        // Setzt die Hintergrundfarbe des GL-Clears auf ein helles Blau
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);// Löscht den Bildschirm mit der festgelegten Hintergrundfarbe
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        float delta = Gdx.graphics.getDeltaTime();
 
-        float delta = Gdx.graphics.getDeltaTime();// Liefert die Zeitdifferenz seit dem letzten Aufruf der render-Methode
-
-
-        // Führt die Spiellogik für jeden Logikframe aus
-        while(deltaAccumulator > logicFrameTime) {
-            deltaAccumulator -= logicFrameTime;// Reduziert den Akkumulator um die Dauer eines Logik-Frames
-            act(logicFrameTime);// Führt die Spiellogik aus
+        while (deltaAccumulator > logicFrameTime) {
+            deltaAccumulator -= logicFrameTime;
+            act(logicFrameTime);
         }
 
-        draw();// Zeichnet die Spielobjekte
+        player.act(delta);
+
+        draw();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-    }// Gibt den SpriteBatch-Ressourcen frei
+    }
 
     @Override
-    public void resize(int width, int height){
-        viewport.update(width,height);
-    }// Aktualisiert den Viewport bei einer Größenänderung des Fensters
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.UP) {
+            player.moveUp();
+        } else if (keycode == Input.Keys.DOWN) {
+            player.moveDown();
+        } else if (keycode == Input.Keys.LEFT) {
+            player.moveLeft();
+        } else if (keycode == Input.Keys.RIGHT) {
+            player.moveRight();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (keycode == Input.Keys.W) {
+            player.stopMovingUp();
+        } else if (keycode == Input.Keys.S) {
+            player.stopMovingDown();
+        } else if (keycode == Input.Keys.A) {
+            player.stopMovingLeft();
+        } else if (keycode == Input.Keys.D) {
+            player.stopMovingRight();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
 }
--- core/src/main/java/at/campus02/swd/game/gameobjects/Player.java
+++ core/src/main/java/at/campus02/swd/game/gameobjects/Player.java
@@ -8,29 +8,75 @@ public class Player implements GameObject {
     private Texture image; // Die Textur des Spielers.
     private Sprite sprite; // Der Sprite des Spielers.
     private String imagePath;
+    private float x; // X-Koordinate des Spielers
+    private float y; // Y-Koordinate des Spielers
+    private float speed; // Geschwindigkeit des Spielers
+
     public Player(String imagePath) {
-        this.imagePath=imagePath;
-        this.image =AssetRepository.getInstance().getTexture(imagePath);
+        this.imagePath = imagePath;
+        this.image = AssetRepository.getInstance().getTexture(imagePath);
         this.sprite = new Sprite(image);
+        this.x = 0;
+        this.y = 0;
+        this.speed = 5; // Beispielgeschwindigkeit: 5 Einheiten pro Aktualisierungsschritt
     }
-    // Initialisiert die Textur des Spielers mit dem angegebenen Bildpfad.
-    // Erstellt einen Sprite mit der Textur des Spielers.
+
     @Override
     public void act(float delta) {
+        // Hier können Sie die Bewegungslogik des Spielers implementieren, die in jedem Aktualisierungsschritt aufgerufen wird.
+        // Die delta-Zeit kann verwendet werden, um die Bewegung des Spielers proportional zur Zeit zu machen.
 
+        // Beispiel: Spieler nach oben bewegen
+        y += speed * delta;
     }
-    // Die act-Methode wird von GameObject implementiert, hat aber hier keine Funktionalität.
-    // Es wird nichts im Spieler selbst aktualisiert.
+
+    // Weitere Methoden zur Steuerung der Bewegung hinzufügen
+    public void moveUp() {
+        y += speed;
+        sprite.setPosition(x, y);
+    }
+
+    public void moveDown() {
+        y -= speed;
+        sprite.setPosition(x, y);
+    }
+
+    public void moveLeft() {
+        x -= speed;
+        sprite.setPosition(x, y);
+    }
+
+    public void moveRight() {
+        x += speed;
+        sprite.setPosition(x, y);
+    }
+
+    // Weitere Methoden zur Steuerung der Bewegung hinzufügen, um die Bewegung zu stoppen
+    public void stopMovingUp() {
+        // Optional: Hier können Sie zusätzlichen Code hinzufügen, um das Stoppen der Bewegung zu behandeln
+    }
+
+    public void stopMovingDown() {
+        // Optional: Hier können Sie zusätzlichen Code hinzufügen, um das Stoppen der Bewegung zu behandeln
+    }
+
+    public void stopMovingLeft() {
+        // Optional: Hier können Sie zusätzlichen Code hinzufügen, um das Stoppen der Bewegung zu behandeln
+    }
+
+    public void stopMovingRight() {
+        // Optional: Hier können Sie zusätzlichen Code hinzufügen, um das Stoppen der Bewegung zu behandeln
+    }
 
     @Override
     public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
         sprite.setPosition(x, y);
     }
-    // Setzt die Position des Sprites auf die angegebenen Koordinaten.
 
     @Override
     public void draw(SpriteBatch batch) {
         sprite.draw(batch);
     }
-    // Zeichnet den Sprite auf den SpriteBatch, der als Parameter übergeben wird.
 }
